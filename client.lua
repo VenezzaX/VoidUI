ebebf7b6c503b4cd98dc6157e30ae2154d3e927b
VenezzaX <piozada243@gmail.com>
2026-06-11 03:41:50 -0300

    Update loadstring to fetch client.lua from repository

--- client.lua
+++ client.lua
@@ -586,7 +586,7 @@ local function setupAutoReinject()
             loadstring(readfile("Script.lua"))()
         else
             local ok, err = pcall(function()
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/VenezzaX/VoidUI/refs/heads/main/menu.lua", true))()
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/VenezzaX/VoidUI/refs/heads/main/client.lua", true))()
             end)
             if not ok then
                 warn("Void Utility Hub auto-reinject failed: " .. tostring(err))
